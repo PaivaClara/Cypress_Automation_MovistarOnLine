2827b6a18a735349fd5602378f543e1a6b1bf597
Clara Paiva <paivaclara1998@gmail.com>
2023-11-27 01:31:05 -0300

    Pruebas realizadas Final

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "6pcas7",
+  projectId: "ej65r7",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
